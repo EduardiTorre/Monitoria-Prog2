27852b72a388fae71be289b0287a4611b311c315
EduardiTorre <eduardozapera@gmail.com>
2022-03-12 21:41:30 -0300

    demonstracao lib pessoal

---
+++ libjeoc.c
@@ -0,0 +1,43 @@
+/* ************************************************************************ *
+ *    libjeoc.c, VERSION 1                                                  *
+ *    parte da biblioteca onde se coloca as funcoes criadas                 *   
+ *                                                                          *
+ *    Copyright (C) 2022 by Jose Eduardo Oliveira Cavalcanti Lupa           *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ *  To contact the author, please write to:                                 *
+ *  Name: Jose Eduardo Oliveira Cavalcanti Lupa <jeoc@poli.br>              *
+ *  Webpage: http://beco.poli.br/~jeoc                                      *
+ *  Phone: (81) 99686-9333                                                  *
+ * ************************************************************************ *
+ *
+ */
+
+#include "libjeoc.h"
+
+/* criacao de funcao que soma simples para demonstrar a lib pessoal */
+int soma_dois(int a, int b){
+	int soma;
+
+	soma = a + b; 
+
+	return soma;
+} 
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
---
+++ libjeoc.h
@@ -0,0 +1,49 @@
+/* ************************************************************************ *
+ *    libjeoc.h, VERSION 1                                                  *
+ *    biblioteca pessoal para programaçao 2                                 *
+ *                                                                          *
+ *    Copyright (C) 2022 by Jose Eduardo Oliveira Cavalcanti Lupa           *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ *  To contact the author, please write to:                                 *
+ *  Name: Jose Eduardo Oliveira Cavalcanti Lupa <jeoc@poli.br>              *
+ *  Webpage: http://beco.poli.br/~jeoc                                      *
+ *  Phone: (81) 99686-9333                                                  *
+ * ************************************************************************ *
+ *
+ */
+
+#ifndef libjeoc_biblioteca_h
+#define libjeoc_biblioteca_h
+
+/* includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+
+/* prototipos */
+int soma_dois(int a, int b);
+
+
+
+#endif                                  // final da biblio pessoal
+
+/* ---------------------------------------------------------------------- */
+/* vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql : C config for Vim modeline */
+/* Template by Dr. Beco <rcb at beco dot cc> Version 20160612.142044      */
